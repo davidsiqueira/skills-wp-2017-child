b3af5e01013221d9ddd57debb460f0e9a22e0320
David Siqueira Pereira <dvdleaf@gmail.com>
2017-08-21 12:28:07 +0100

    Ajustes no tema

--- functions.php
+++ functions.php
@@ -4,3 +4,13 @@ add_action( 'wp_enqueue_scripts', 'enqueue_parent_styles' );
 function enqueue_parent_styles() {
    wp_enqueue_style( 'parent-style', get_template_directory_uri().'/style.css' );
 }
+
+register_sidebar( array(
+    'name'          => __( 'Footer 3', 'twentyseventeen' ),
+    'id'            => 'sidebar-4',
+    'description'   => __( 'Add widgets here to appear in your footer.', 'twentyseventeen' ),
+    'before_widget' => '<section id="%1$s" class="widget %2$s">',
+    'after_widget'  => '</section>',
+    'before_title'  => '<h2 class="widget-title">',
+    'after_title'   => '</h2>',
+) );
---
+++ template-parts/footer/site-info.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Displays footer site info
+ *
+ * @package WordPress
+ * @subpackage Twenty_Seventeen
+ * @since 1.0
+ * @version 1.0
+ */
+
+?>
+<div class="site-info">
+	<div class="info-footer">
+			PRIMAVERA Business Software Solutions, S.A.</br>
+			© 2017 - Todos os Direitos Resevados
+	</div>
+	<!-- <a href="<?php echo esc_url( __( 'https://wordpress.org/', 'twentyseventeen' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'twentyseventeen' ), 'WordPress' ); ?></a> -->
+</div><!-- .site-info -->
